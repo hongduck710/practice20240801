fd22cac05990f5b6027b885d81b538093d119ad4
hongduck710 <hongik710@gmail.com>
2024-09-12 15:08:37 +0900

    20240912 브라우저 콘솔창에 fileName이 null이 되는 부분 수정!!!!!

--- practice20240801/src/main/java/org/zerock/controller/UploadController.java
+++ practice20240801/src/main/java/org/zerock/controller/UploadController.java
@@ -28,110 +28,108 @@ import net.coobird.thumbnailator.Thumbnailator;
 @Log4j
 public class UploadController {
 	
-	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile){
-		
-		List<AttachFileDTO> list = new ArrayList<>();
-		String uploadFolder = "C:\\zzz\\upload";
-		
-		String uploadFolderPath = getFolder();
-		// make folder -------------------------
-		File uploadPath = new File(uploadFolder, uploadFolderPath);
-		
-		if(uploadPath.exists() == false) {
-			uploadPath.mkdirs();
-		}
-		// make yyyy/MM/dd folder
-		
-		for (MultipartFile multipartFile : uploadFile) {
-			
-			AttachFileDTO attachDTO = new AttachFileDTO();
-			
-			String uploadFileName = multipartFile.getOriginalFilename();
-			
-			// IE has file path
-			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
-			log.info("파일 이름만(only file name): " + uploadFileName);
-			
-			UUID uuid = UUID.randomUUID();
-			
-			uploadFileName = uuid.toString() + "_" + uploadFileName;
-			
-			try {
-				File saveFile = new File(uploadPath, uploadFileName);
-				multipartFile.transferTo(saveFile);
-				
-				attachDTO.setUuid(uuid.toString());
-				attachDTO.setUploadPath(uploadFolderPath);
-				
-				// check image type file
-				if (checkImageType(saveFile)) {
-					
-					attachDTO.setImage(true);
-					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
-					
-					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
-					
-					thumbnail.close();
-				}
-				
-				//add to List
-				list.add(attachDTO);
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			} // try catch 닫기 
-			
-			
-		} // for구문 닫기 
-		
-		return new ResponseEntity<>(list, HttpStatus.OK);
-	}
-	
-	private boolean checkImageType(File file) { // 특정한 파일이 이미지 타입인지 검사
+	/* 이미지 파일 여부 체크 */
+	private boolean checkImageType(File file) {
 		try {
 			String contentType = Files.probeContentType(file.toPath());
 			return contentType.startsWith("image");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 		return false;
-	}
+	} // checkImageType 닫음
 	
-	private String getFolder() {
-		
+	/*날짜별로  폴더 만들기 위한 메소드*/
+	private String getFolder () {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		Date date = new Date();
 		String str = sdf.format(date);
 		return str.replace("-", File.separator);
-	}
+	} // getFolder 닫음
 	
-	@GetMapping("/uploadAjax")
-	public void uploadAjax() {
-		log.info("업로드 AJAX(upload ajax)");
-	}
-/*	
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_VALUE) //수정
+	@ResponseBody // @ResponseBody 추가
+	public ResponseEntity<List<AttachFileDTO>> // void지우고 ResponseEntity타입으로 수정
+	uploadAjaxPost(MultipartFile[] uploadFile) {
+		log.info("업데이트 AJAX 포스트(update ajax post)....");
+		
+		List<AttachFileDTO> list = new ArrayList<>(); // 추가
+		String uploadFolder = "C:\\zzz\\upload";
+		
+		String uploadFolderPath = getFolder(); // 추가 
+		// make folder --------------------
+		File uploadPath = new File(uploadFolder, uploadFolderPath); // getFolder()를 uploadFolderPath로 수정
+		log.info("업로드 패스(upload path): " + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		} // make yyyy/MM/dd folder
+		
+		for (MultipartFile multipartFile : uploadFile) {
+			log.info("--------------------------");
+			log.info("업로드 파일 이름(upload file name): " + multipartFile.getOriginalFilename());
+			log.info("업로드 파일 사이즈(upload file size): " + multipartFile.getSize());
+			
+			AttachFileDTO attachDTO = new AttachFileDTO(); // 추가
+			
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			// IE has file path
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+			log.info("파일 이름만(only file name): " + uploadFileName);
+			attachDTO.setFileName(uploadFileName); // 20240912.: 작일(昨日)에 해당 코드가 누락되어서 json에서 fileName이 null인 상태로 전송됨
+			
+			// 파일 이름에 uuid 붙임 
+			UUID uuid = UUID.randomUUID();
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			try {
+				File saveFile = new File(uploadPath, uploadFileName); // try catch문 위에 있던 saveFIle을 try안으로 넣음
+				multipartFile.transferTo(saveFile);
+				
+				attachDTO.setUuid(uuid.toString()); // 추가
+				attachDTO.setUploadPath(uploadFolderPath); // 추가
+				
+				// check imagr type file
+				if(checkImageType(saveFile)) {
+					
+					attachDTO.setImage(true); // 추가
+					
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+					thumbnail.close();
+				} // if문 닫음
+				
+				// add to List
+				list.add(attachDTO); // 추가
+				
+			} catch(Exception e){
+				log.error(e.getMessage());
+			} // catch 닫음
+			
+		} // for 닫음
+		return new ResponseEntity<>(list, HttpStatus.OK);		
+	} // uploadAjaxPost닫음
+		
+/*		
 	@PostMapping("/uploadAjaxAction")
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
-		log.info("업데이트 AJAX 포스트(update ajax post)...........");
+		log.info("업데이트 AJAX 포스트(update ajax post)....");
 		
 		String uploadFolder = "C:\\zzz\\upload";
 		
-		// make folder -----------------
+		// make folder --------------------
 		File uploadPath = new File(uploadFolder, getFolder());
 		log.info("업로드 패스(upload path): " + uploadPath);
 		
 		if(uploadPath.exists() == false) {
 			uploadPath.mkdirs();
-		}
-		//make yyyy/MM/dd folder
+		} // make yyyy/MM/dd folder
 		
-		for(MultipartFile multipartFile : uploadFile) {
-			log.info("---------------------------------------");
-			log.info("업로드 파일 이름(Upload File Name): " + multipartFile.getOriginalFilename());
-			log.info("업로드 파일 사이즈(Upload File Size): " + multipartFile.getSize());
+		for (MultipartFile multipartFile : uploadFile) {
+			log.info("--------------------------");
+			log.info("업로드 파일 이름(upload file name): " + multipartFile.getOriginalFilename());
+			log.info("업로드 파일 사이즈(upload file size): " + multipartFile.getSize());
 			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
@@ -139,34 +137,37 @@ public class UploadController {
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 			log.info("파일 이름만(only file name): " + uploadFileName);
 			
+			// 파일 이름에 uuid 붙임 
 			UUID uuid = UUID.randomUUID();
-			
 			uploadFileName = uuid.toString() + "_" + uploadFileName;
 			
-			// File saveFile = new File(uploadFolder, uploadFileName);
-			
 			try {
-				File saveFile = new File(uploadPath, uploadFileName);
+				File saveFile = new File(uploadPath, uploadFileName); // try catch문 위에 있던 saveFIle을 try안으로 넣음
 				multipartFile.transferTo(saveFile);
 				
-				//check image type file
+				// check imagr type file
 				if(checkImageType(saveFile)) {
 					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
 					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
 					thumbnail.close();
-				}
+				} // if문 닫음
 				
-			} catch (Exception e) {
+			} catch(Exception e){
 				log.error(e.getMessage());
 			} // catch 닫음
 			
 		} // for 닫음
-		
-	}
+				
+	} // uploadAjaxPost닫음
 */	
+	@GetMapping("/uploadAjax")
+	public void uploadAjax () {
+		log.info("업로드 AJAX페이지(upload ajax)");
+	}
+	
 	@GetMapping("/uploadForm")
 	public void uploadForm() {
-		log.info("업로드 폼(upload form)");
+		log.info("업로드폼(upload form)");
 	}
 	
 	@PostMapping("/uploadFormAction")
@@ -174,22 +175,21 @@ public class UploadController {
 		
 		String uploadFolder = "C:\\zzz\\upload";
 		
-		for(MultipartFile multipartFile : uploadFile) {
-			log.info("--------------------------------");
+		for (MultipartFile multipartFile : uploadFile) {
+			log.info("---------------------------------");
 			log.info("업로드 파일 이름(Upload File Name): " + multipartFile.getOriginalFilename());
 			log.info("업로드 파일 사이즈(Upload File Size): " + multipartFile.getSize());
 			
 			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
-			
 			try {
 				multipartFile.transferTo(saveFile);
 			} catch (Exception e) {
 				log.error(e.getMessage());
-			} //catch 닫음
+			} // catch 닫음
 			
-		} // for구문 닫음
-	}
+		} // for 닫음
+	} // uploadFormPost 닫음
 	
-	
+
 	
 }
--- practice20240801/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ practice20240801/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -10,73 +10,66 @@
 <title>업로드 AJAX</title>
 </head>
 <body>
-	<h1>UPLOAD WITH AJAX</h1>
-	<div class="uploadDiv">
-		<input type="file" name="uploadFile" multiple />
-	</div>	
-	<button id="uploadBtn">UPLOAD</button>
+
+<h1>UPLOAD WITH AJAX</h1>
+<div class="uploadDiv">
+	<input type="file" name="uploadFile" multiple />
+</div>
 	
+<button id="uploadBtn">UPLOAD</button>
 	
-	
-	
-<!-- 	<script src="https://code.jquery.com/jquery-3.7.1.min.js"  type="text/javascript"></script> --><!--  20240911: header.jspf에 있는 제이쿼리 CDN을 첨부하였더니 ajax is not a function오류 사라짐, slim버전이 아닌 CDM을 사용하였더니 ajax is not a function오류 사라짐-->
-	<script
-			  src="https://code.jquery.com/jquery-3.7.1.min.js"
-			  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
-			  crossorigin="anonymous">
-	</script><!-- 20240911: integrity, crossoorigin 지우지 않으면 image파일 여부 true, false에 영향이 있는지 없는 지 확인하기 위해 지우지 않고 제이쿼리 첨부함 -->
-	<script>
-	
+<!--<script src="https://code.jquery.com/jquery-3.7.1.min.js"  type="text/javascript"></script>--><!--  20240911: header.jspf에 있는 제이쿼리 CDN을 첨부하였더니 ajax is not a function오류 사라짐, slim버전이 아닌 CDM을 사용하였더니 ajax is not a function오류 사라짐-->
+<script
+	src="https://code.jquery.com/jquery-3.7.1.min.js"
+	integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+	crossorigin="anonymous"></script><!-- 20240911: integrity, crossoorigin 지우지 않으면 image파일 여부 true, false에 영향이 있는지 없는 지 확인하기 위해 지우지 않고 제이쿼리 첨부함 -->
+
+<script>
+$(document).ready(function(){
 	let regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
 	let maxSize = 5242880; // 5MB
-	
-	function checkExtension(fileName, fileSize) {
-		
-		if(fileSize > maxSize) {
+
+	function checkExtension (fileName, fileSize) {
+		if (fileSize >= maxSize) {
 			alert("파일 사이즈 초과");
 			return false;
 		}
-		
+
 		if(regex.test(fileName)) {
 			alert("해당 종류의 파일은 업로드 할 수 없습니다.");
 			return false;
 		}
-		
 		return true;
-	}
-	
-	$(document).ready(function(){
-		$("#uploadBtn").on("click", function(e){
-			let formData = new FormData(); /* 202409011 정리:FormData객체는 가상의 <form>태그와 같다고 생각하면 됨. Ajax를 이용하는 파일 업로드는 FormData를 이용해서 필요한 파라미터를 담아서 전송하는 방식(FormData객체는 브라우저의 제약이 있으므로 주의) */
-			let inputFile = $("input[name='uploadFile']");
-			let files = inputFile[0].files;
-			
-			console.log(files);
-			
-			// add File Data to formData
-			for(let i = 0; i < files.length; i++) {
-				
-				if( !checkExtension(files[i].name, files[i].size) ) {
-					return false;
-				}
-				
-				formData.append("uploadFile", files[i]);
+	} // checkExtension 닫음
+
+	$("#uploadBtn").on("click", function(e){
+		let formData = new FormData(); /* 202409011 정리:FormData객체는 가상의 <form>태그와 같다고 생각하면 됨. Ajax를 이용하는 파일 업로드는 FormData를 이용해서 필요한 파라미터를 담아서 전송하는 방식(FormData객체는 브라우저의 제약이 있으므로 주의) */
+		let inputFile = $("input[name='uploadFile']");
+		let files = inputFile[0].files;
+		console.log(files);
+
+		// add File Data to formData
+		for(let i = 0; i < files.length; i++){
+			if ( !checkExtension(files[i].name, files[i].size) ) {
+				return false;
 			}
-			
-			$.ajax({
-				url : '/uploadAjaxAction',
-				processData : false,
-				contentType : false,
-				data : formData,
-				type : 'POST',
-				dadaType: 'json',
-				success : function(result) {
-					console.log(result);
-				} 
-			}); // ajax 닫음
-			
-		}); // #uploadBtn클릭 닫음
-	});	
-	</script>
+			formData.append("uploadFile", files[i]);
+		}
+
+		$.ajax({
+			url: "/uploadAjaxAction",
+			processData : false,
+			contentType : false,
+			data : formData,
+			type : "POST",
+			dataType : "json",
+			success : function(result){
+				console.log(result);
+			}
+		}); // $.ajax
+	}); 
+});
+
+</script>
 </body>
 </html>    
